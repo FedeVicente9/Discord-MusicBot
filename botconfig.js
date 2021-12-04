a3b59213e3dd42bad60157d5d2de670d0f08df82
FedeVicente9 <72765708+FedeVicente9@users.noreply.github.com>
2021-12-03 21:06:39 -0300

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -28,9 +28,9 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "lava.link",
-    port: 80, // The port that lavalink is listening to. This must be a number!
-    pass: "your password",
+    host: "lavalink.pumpdev.org",
+    port: 3876, // The port that lavalink is listening to. This must be a number!
+    pass: "pumpisfree",
     retryAmount: 5, // - Optional
     retryDelay: 1000, // - Optional
     secure: false, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
